3b949473e9e6064ca659618975e97b0b0334454f
kabilank6 <kabilan-k@hotmail.com>
2020-07-26 15:41:01 +1200

    Cleaned type errors
    Dealt with compiler issues

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Paramore from './Paramore.jpg'
+import U2 from './U2.jpg'
 
 interface IState {
   chosen: any,
   artist: any,
   song: any,
-  lyrics: any
+  lyrics: any,
 }
 
 class App extends React.Component<{},IState>{
@@ -15,7 +17,7 @@ class App extends React.Component<{},IState>{
     song: "",
     lyrics: ""
   }
-  handleTryAgain = (e) => {
+  handleTryAgain = (e: any) => {
     e.preventDefault();
     this.setState({
         chosen: "0",
@@ -24,22 +26,17 @@ class App extends React.Component<{},IState>{
         lyrics: ""
     })
   }
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value
-    })
-  }
-  handleArtistChange = (e) => {
+  handleArtistChange = (e: any) => {
     this.setState({
       artist: e.target.value
     })
   }
-  handleSongChange = (e) => {
+  handleSongChange = (e: any) => {
     this.setState({
       song: e.target.value
     })
   }
-  handleSubmit = (e) => {
+  handleSubmit = (e: any) => {
     e.preventDefault();
     const text = {
         song : this.state.song,
@@ -51,7 +48,7 @@ class App extends React.Component<{},IState>{
     fetch(url)
     .then(response => response.json())
     .then(data => {
-      console.log(data) // Prints result from `response.json()` in getRequest
+      console.log(data) 
       const items = data;
       console.log(items.lyrics);
       current.setState({
@@ -65,23 +62,31 @@ class App extends React.Component<{},IState>{
     render() {
       if (this.state.chosen === "0") {
         return (
-          <div className="center">
-            <h1>Hello world!</h1>
-            <form onSubmit={this.handleSubmit}>
-              <label>Pick a song (For example, Ain't it Fun).</label>
-              <input type="text" id="song" onChange={this.handleChange} value={this.state.song} />
-              <label>Who is the song's artist? (For example, Paramore)</label>
-              <input type="text" id="artist" onChange={this.handleChange} value={this.state.artist} />
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+          <div className="container">
+            <div className="center">
+              <h1>Why, hello there! It's awesome to see you here</h1>
+              <h4>Have you ever wondered what the lyrics of your favourite song are? 
+                If so, please feel free to search them up below (of course, we will find them for you ;)).</h4>
+                <img src={Paramore} className="center" alt="Ain't it fun"/>
+              <h6>Disclaimer: Please be sure to put the artist and song in correctly, otherwise we may be looking for something that does not exist!</h6>
+              <form onSubmit={this.handleSubmit}>
+                <label>Pick a song (For example, Ain't it Fun).</label>
+                <input type="text" id="song" onChange={this.handleSongChange} value={this.state.song} />
+                <label>Who is the song's artist? (For example, Paramore)</label>
+                <input type="text" id="artist" onChange={this.handleArtistChange} value={this.state.artist} />
+                <button type="submit" className="btn btn-primary">Submit</button>
+              </form>
+            </div>
           </div>
         );
       }
       else if (this.state.chosen === "1") {
         return (
           <div className="center">
-            <h1>Below are the lyrics of {this.state.song}, by {this.state.artist}, as you like it!</h1>
+            <h1>Below are the lyrics of "{this.state.song}", by {this.state.artist}, as you like it!</h1>
             <h4>{this.state.lyrics}</h4>
+            <img src={U2} className="center" alt="The Miracle of Joey Ramone"/>
+            <h6>Keen to try again? The door is open</h6>
             <button className="btn btn-primary" onClick={this.handleTryAgain}>Try Again</button>
           </div>
         )
